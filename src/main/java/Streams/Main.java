0e7b54be07e2c81ee9688678ebdd32008371afcb
AbduLaziz <l.komilov@axonlogic.uz>
2025-01-19 19:07:43 +0500

    Last Element

--- src/main/java/Streams/Main.java
+++ src/main/java/Streams/Main.java
@@ -1,7 +1,20 @@
 package Streams;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<String> list = new ArrayList<String>();
+        list.add("Alish");
+        list.add("Valish");
+        list.add("Toshmat");
+        list.add("Eshmat");
+        Optional<String>optional=list.stream().skip(list.size()-1).findFirst();
+
+        System.out.println(optional.get());
+
     }
 }
---
+++ src/main/java/Streams/Streams_Exercises.java
@@ -0,0 +1,9 @@
+package Streams;
+
+public interface Streams_Exercises {
+
+     static void lost(String value){
+         System.out.println(value);
+     }
+
+}
